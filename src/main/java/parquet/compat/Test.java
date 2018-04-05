1357ebee11b474bf9a9aca9ba5face8a25d4f760
ACA <aca@ACAs-MacBook-Pro.local>
2018-04-05 17:24:55 +0530

    2nd commit

--- src/main/java/parquet/compat/Test.java
+++ src/main/java/parquet/compat/Test.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 public class Test {
 
 	public static void main(String[] args) {
-		File parquetFile = new File("/Users/aca/Desktop/Pawan/code_base/parquet-compatibility-master/parquet-testdata/tpch/customer.parquet");
-		File csvOutputFile = new File("/Users/aca/Desktop/Pawan/code_base/parquet-compatibility-master/parquet-testdata/tpch/customer1.csv");
+	//	String parquetFileName="userdata1.parquet";
+		String parquetFileName="part-r-00000.snappy.parquet";
+		String outputCSVFileName="click.csv";
+		File parquetFile = new File("/Users/aca/Desktop/Pawan/code_base/ParquetToJson/test_data/"+parquetFileName);
+		File csvOutputFile = new File("/Users/aca/Desktop/Pawan/code_base/ParquetToJson/test_data/"+outputCSVFileName);
 		try {
 			//ConvertUtils.convertCsvToParquet(csvOutputFile,parquetFile);
 			ConvertUtils.convertParquetToCSV(parquetFile, csvOutputFile);
